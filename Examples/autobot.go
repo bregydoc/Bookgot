2e7fb06c85afe2e1c102756da9333284310dc953
bregydoc <bregy.malpartida@utec.edu.pe>
2016-08-10 14:54:53 -0500

    Added example

--- Examples/autobot.go
+++ Examples/autobot.go
@@ -1,1 +1,31 @@
 package main
+
+import(
+    "github.com/bregydoc/Bookgot"
+    "fmt"
+)
+
+func main() {
+    userExample := Bookgot.CreateNewPBUser("example@domain.com", "password")
+    if userExample.Login() {
+        fmt.Println("Logged!")
+    }
+    for {
+        haveNewBook := userExample.VerifyIfIHaveLastFreeBook()
+
+        if haveNewBook{
+            haveNewBook = userExample.VerifyIfIHaveLastFreeBook()
+            timeAfter, _ := Bookgot.GetTimeForNewFreeBook()
+            fmt.Println("Not new free book, time for next book: ", timeAfter.String())
+        }else {
+            petition :=  userExample.PullNewFreeBook()
+            if petition{
+                fmt.Println("New free book added at your library")
+            }else{
+                fmt.Println("Error, verify your email and password")
+            }
+        }
+
+    }
+
+}
